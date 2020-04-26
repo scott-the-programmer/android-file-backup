d4e5b2d0c2be95b809a5256254586614cd7e123d
Scott Murray <scottam.coder@gmail.com>
2020-04-26 15:17:47 +1200

    fix unit tests for android.test

--- test/repositories/android.test.ts
+++ test/repositories/android.test.ts
@@ -1,7 +1,5 @@
 import { AndroidRepository } from "../../src/repositories/android";
 import * as fs from "fs";
-import * as ncp from "ncp";
-
 import { CheckPoint } from "../../src/models/checkpoint";
 import * as sinon from "sinon";
 import { expect } from "chai";
@@ -14,11 +12,16 @@ describe("android folder repository", () => {
     const checkpoint = new CheckPoint("mock", "mock");
 
     const mkdirStub = sinon.stub(fs,"mkdir");
-    mkdirStub.callsArg(1)
+    mkdirStub.callsArg(2)
 
-    const ncpStub = sinon.stub(ncp,"ncp");
-    ncpStub.callsArg(2)
+
     const repo = new AndroidRepository("mock", checkpoint);
+    const mockFiles = ["mock/path/file1","mock/path/file2","mock/path/file3"]
+    const globStub = sinon.stub(repo,"getFiles").callsFake(()=>{
+      return new Promise((resolve,reject)=>{
+        resolve(mockFiles)
+      })
+    });
 
     //Act
     const backupLocation = await repo.copy("mock/path");
@@ -29,6 +32,38 @@ describe("android folder repository", () => {
     //Reset
     clock.restore();
     mkdirStub.restore();
-    ncpStub.restore();
+    globStub.restore();
+  });
+
+  it("should return correct file count", async () => {
+    //Setup
+    var now = new Date(2020, 1, 1, 1, 1, 1, 1);
+    var clock = sinon.useFakeTimers(now.getTime());
+    const checkpoint = new CheckPoint("mock", "mock");
+
+    const mkdirStub = sinon.stub(fs,"mkdir");
+    mkdirStub.callsArg(2)
+
+
+    const repo = new AndroidRepository("mock", checkpoint);
+    const mockFiles = ["mock/path/file1","mock/path/file2","mock/path/file3"]
+    const globStub = sinon.stub(repo,"getFiles").callsFake(()=>{
+      return new Promise((resolve,reject)=>{
+        resolve(mockFiles)
+      })
+    });
+
+    //Act
+    const fileCount = await repo.getFileCount();
+
+    //Assert
+    expect(fileCount).to.equal(3);
+
+    //Reset
+    clock.restore();
+    mkdirStub.restore();
+    globStub.restore();
   });
 });
+
+
