4baa41d97621d3c7af2f4a03bcd638a90c46b85e
Scott Murray <scottam.coder@gmail.com>
2020-04-27 19:06:27 +1200

    minor update to docstring

--- src/cli-ui/progress-value.ts
+++ src/cli-ui/progress-value.ts
@@ -2,9 +2,10 @@ import cli from "cli-ux";
 import { Progress } from "./progress";
 
 export class ProgressValue extends Progress {
+
   /**
-   * Responsible for creating and controlling the CLI progress value
-   * @param targetValue value to progress towards (see https://github.com/oclif/cli-ux)
+   * Responsible for controlling progress for a simple scalar value
+   * @param prefixMessage Message to prefix the value with 
    */
   constructor(prefixMessage: string) {
     super(
